291b335612604dc3d1cda4dfd603fa742d3e4383
alfigufron <alfigufron21@gmail.com>
2021-11-14 13:12:59 +0700

    [CREATE] Navbar Component

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,19 @@
+const btnToggle = document.getElementById('btn-toggle');
+const navContent = document.getElementById('nav-content');
+
 // Resize Screen
 window.addEventListener('resize', function () {
-  
+  sidebarSizeScreen();
 });
+
+btnToggle.addEventListener('click', function () {
+  navContent.classList.toggle('mobile-toggled');
+})
+
+function sidebarSizeScreen() {
+  const width = window.innerWidth;
+
+  if (width > 1200) {
+    navContent.classList.remove('mobile-toggled');
+  }
+}
